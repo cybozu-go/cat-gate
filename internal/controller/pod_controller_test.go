12bef89ef1cbcd2c63ac9a167730954a81887901
TAK848 <pairs_char0j@icloud.com>
2023-09-01 17:32:50 +0900

    add createNewPod to controller test
    Signed-off-by: TAK848 <pairs_char0j@icloud.com>

--- internal/controller/pod_controller_test.go
+++ internal/controller/pod_controller_test.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cybozu-go/cat-gate/internal/constants"
 	. "github.com/onsi/ginkgo/v2"
@@ -15,7 +16,7 @@ var _ = Describe("CatGate controller", func() {
 
 	ctx := context.Background()
 
-	It("should remove scheduling gate", func() {
+	It("should remove scheduling gate when the number of pods is 1", func() {
 		newPod := &corev1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
 				Namespace: "default",
@@ -46,4 +47,52 @@ var _ = Describe("CatGate controller", func() {
 			g.Expect(pod.Spec.SchedulingGates).NotTo(ConsistOf(corev1.PodSchedulingGate{Name: constants.PodSchedulingGateName}))
 		}).Should(Succeed())
 	})
+
+	It("should remove 1 scheduling gate when the number of pods is 8", func() {
+		testName := "multiplepods"
+		namespace := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: testName,
+			},
+		}
+		err := k8sClient.Create(ctx, namespace)
+		Expect(err).NotTo(HaveOccurred())
+
+		for i := 0; i < 8; i++ {
+			createNewPod(i, testName)
+		}
+		pods := &corev1.PodList{}
+		Eventually(func(g Gomega) {
+			err := k8sClient.List(ctx, pods)
+			g.Expect(err).NotTo(HaveOccurred())
+			// TODO: 全部待つ
+		})
+
+	})
 })
+
+func createNewPod(index int, testName string) {
+	newPod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testName,
+			Name:      fmt.Sprintf("%s-pod-%d", testName, index),
+		},
+		Spec: corev1.PodSpec{
+			InitContainers: []corev1.Container{
+				{
+					Name:  "sample1",
+					Image: testName + ".example.com/sample1-image:1.0.0",
+				},
+			},
+			Containers: []corev1.Container{
+				{
+					Name:  "sample2",
+					Image: testName + ".example.com/sample2-image:1.0.0",
+				},
+			},
+			Hostname: fmt.Sprintf("%s-hostname-%d", testName, index),
+		},
+	}
+	err := k8sClient.Create(ctx, newPod)
+	Expect(err).NotTo(HaveOccurred())
+}
